6e5492382421f952bfd2fa15526c055add201d3b
Santosh M <128308323+SantoshMorla@users.noreply.github.com>
2024-04-23 14:25:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,12 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
+resource "aws_s3_bucket" "bucket_Trane" {
+  bucket = var.bucket_Trane
   acl    = "public-read"
 
   tags = {
-    Name        = var.bucket_name
+    Name        = var.bucket_Trane
     Environment = "Devlopment"
   }
 }
